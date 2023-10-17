9536424eb2ef34d9daf918f213be04df97681da7
Eliott-3 <113546109+Eliott-3@users.noreply.github.com>
2023-10-17 11:39:56 -0600

    Administrador

--- src/Components/NavAdmin.jsx
+++ src/Components/NavAdmin.jsx
@@ -1,28 +1,39 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useState } from 'react';
+import Home from './Home';
+import Login from './Login';
+import AcercaDe from './AcercaDe';
+import Artwork from './Artwork';
+import Media from './Media';
+import Contacto from './Contacto';
+import Admin from './Admin';
 
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes
+} from 'react-router-dom'
 
 function NavAdmin() {
-  const [zapatosRef, setZapatosRef] = useState(null);
-
-  const handleScrollToZapatos = () => {
-    if (zapatosRef && zapatosRef.current) {
-      window.scrollTo({
-        top: zapatosRef.current.offsetTop,
-        behavior: 'smooth',
-      });
-    }
-  };
-
   return (
-    <div className='NavegacionAdmin'>
-      <a href="#" onClick={handleScrollToZapatos}>Zapatos</a>
-    </div>
+   <Router>
+     <nav className="ClassNavBar">
+       <ul className="menu">
+         <li><Link to="/AcercaDeAd">ACERCA DE</Link></li>
+         <li><Link to="/ArtworkAd">ARTWORK</Link></li>
+         <li><Link to="/MediaAd">MEDIA</Link></li>
+       <li><Link to="/ContactoAd">CONTACTO</Link></li>
+       </ul>
+     </nav>
+
+     <Routes>
+       <Route path="/AcercaDeAd" element={<AcercaDe />}></Route>
+       <Route path="/ArtworkAd" element={<Artwork />}></Route>
+       <Route path="/MediaAd" element={<Media />}></Route>
+       <Route path="/ContactoAd" element={<Contacto />}></Route>
+     </Routes>
+   </Router>
   );
 }
-
-// Agregar un método para establecer la referencia desde AdminInfo
-NavAdmin.setZapatosRef = (ref) => {
-  setZapatosRef(ref);
-};
 
 export default NavAdmin;
